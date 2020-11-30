24ebfb17636ba421b5f64f2dd1cc4432efce8f88
Никита <mr.gonk@mail.ru>
2020-11-30 20:30:26 +0500

    initial

--- step_2.py
+++ step_2.py
@@ -1,4 +1,4 @@
-what = input('Что делаем? (+,-):')
+what = input('Что делаем? (+,-,/,*):')
 if what == '+':
     a = float(input('Первое число:'))
     b = float(input('Второе число:'))
@@ -8,6 +8,16 @@ elif what == '-':
     a = float(input('Первое число:'))
     b = float(input('Второе число:'))
     c = a - b
+    print('Результат:'+str(c))
+elif what == '/':
+    a = float(input('Первое число:'))
+    b = float(input('Второе число:'))
+    c = a / b
+    print('Результат:'+str(c))
+elif what == '*':
+    a = float(input('Первое число:'))
+    b = float(input('Второе число:'))
+    c = a * b
     print('Результат:'+str(c))
 else:
     print('Выбрана неверная операция!!!')
