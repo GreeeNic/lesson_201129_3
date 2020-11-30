ad32ae0456a351e04660ee4d147bc7b89e0a8c05
Никита <mr.gonk@mail.ru>
2020-11-30 20:22:19 +0500

    initial

--- step_2.py
+++ step_2.py
@@ -1,1 +1,13 @@
-print('Good for you')
+what = input ('Что делаем? (+,-):')
+if what == '+':
+    a = input('Первое число:')
+    b = input('Второе число:')
+    c = a + b
+    print('Результат='+str(c))
+elif what == '-':
+    a = input('Первое число:')
+    b = input('Второе число:')
+    c = a - b
+    print('Результат='+str(c))
+else:
+    print('Выбрана неверная операция')
